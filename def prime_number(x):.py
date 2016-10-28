2cd6524e7a949fc226b838ef36c414bd09758801
New User Name <tokamau@gmail.com>
2016-10-28 18:49:00 +0300

    function renamed

--- def prime_number(x):.py
+++ def prime_number(x):.py
@@ -1,9 +1,9 @@
-def prime_number(x):
+def prime(x):
 	for x in range (2,x):
-		prime = True
+		prime1 = True
 	for i in range (2,x):
 		if (x % i ==0):
-			prime = False
-	if prime:
-		prime(x)
-prime_number(23)
+			prime1 = False
+	if prime1:
+		prime1(x)
+prime(23)
